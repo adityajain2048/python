2eac719e3965d106a47367bf98abc362c03610b6
adityajain2048 <51382707+adityajain2048@users.noreply.github.com>
2020-11-21 12:24:43 +0530

    Add files via upload

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,13 @@
-import math
-print(math.ceil(2.9))
-print(math.floor(2.2))
+price=1000000
+good_credit=True
+High_salary= False
+
+if good_credit or High_salary:
+
+    down_payment= 0.1*float(price)
+elif good_credit and High_salary:
+    down_payment = 0.05 * float(price)
+else:
+    down_payment= 0.2*float(price)
+
+print(f'{down_payment} is the amount of downpayment')
