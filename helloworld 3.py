0c86287c1bbd65a72518a150bfc42db43f8f1fdc
Aditya <deepika@thisismacbookair-2.local>
2020-11-29 10:16:44 +0530

    m

---
+++ helloworld 3.py
@@ -0,0 +1,11 @@
+from typing import Dict, Union
+
+customer={
+    "name ":"Aditya Jain",
+    "occupation includes ":" coding",
+    "age":18
+
+
+}
+customer["occupation includes "]="coding Eng"
+print(customer["occupation includes "])
