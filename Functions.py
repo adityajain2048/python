e097d03d2c552db656e3992218cba80a4ab93074
Aditya <deepika@thisismacbookair-2.local>
2020-11-29 10:03:03 +0530

    a

---
+++ Functions.py
@@ -0,0 +1,11 @@
+def name(number,numbers):
+    print(numbers)
+    print(number)
+    return number
+
+
+print(" You are a not allowed here")
+name(number=42,numbers="42")
+sqaure = name(42,"54")
+print("you are still not allowed here")
+print(sqaure)
